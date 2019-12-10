124d66daea2b61cb9e0faf0244f8ade3d076e957
heberqc <heberqc@gmail.com>
2019-12-10 17:22:24 -0500

    add cat picture

--- index.js
+++ index.js
@@ -6,21 +6,27 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 const target = document.getElementById("customElement")
+const cat = document.getElementById("cat")
 
 import { fromEvent } from 'rxjs';
 
+const mid_x = 540/2
+const mid_y = 480/2
+
 const observer = {
   next: val => {
-    console.log(val.x, val.y)
+    const angle = Math.atan2(val.y, val.x)
+    console.log(val.x, val.y, angle/3.14159*180)
   },
   error: err => console.log('error', err),
   complete: () => console.log('Complete!')
 };
 
-const source$ = fromEvent(document, 'mousemove');
+const source$ = fromEvent(cat, 'mousemove');
 const subcription = source$.subscribe(observer);
 
 setTimeout(() => {
   subcription.unsubscribe();
   console.log("unsubscribed")
-}, 3000);
+}, 10000);
+
