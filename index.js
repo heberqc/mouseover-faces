abb6c89243bad182855b2d8a721a8fbf131f7358
heberqc <heberqc@gmail.com>
2019-12-25 17:12:37 -0500

    calculate the angle

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+console.clear();
 
-const target = document.getElementById("customElement")
-const cat = document.getElementById("cat")
+const subject = document.getElementById('subject');
 
 import { fromEvent } from 'rxjs';
 
-const mid_x = 540/2
-const mid_y = 480/2
+const X_0 = (1/2) * window.innerWidth;
+const Y_0 = (1/2) * window.innerHeight;
+
+const gap = (x, y) => x < 0 ? 180 : y < 0 ? 360 : 0;
 
 const observer = {
   next: val => {
-    const angle = Math.atan2(val.y, val.x)
-    console.log(val.x, val.y, angle/3.14159*180)
+    const cx = val.x - X_0;
+    const cy = Y_0 - val.y;
+    console.log(`(${cx}, ${cy})`, Math.round(Math.atan(cy/cx)*180/Math.PI + gap(cx, cy)));
   },
   error: err => console.log('error', err),
   complete: () => console.log('Complete!')
 };
 
-const source$ = fromEvent(cat, 'mousemove');
+const source$ = fromEvent(document, 'mousemove');
 const subcription = source$.subscribe(observer);
 
 setTimeout(() => {
